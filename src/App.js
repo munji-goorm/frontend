71c194d506956908245bd0c4cc25e9561250fcc5
oduodg <werewildfire@gmail.com>
2022-11-07 23:30:14 +0900

    :lipstick: Install tailwindcss and apply the font-Kyobo
    tailwindcss 설치 및 교보손글씨체 폰트 적용

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="font-Kyobo">
           Hello, 먼지구름!
         </p>
       </header>
