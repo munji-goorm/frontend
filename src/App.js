d81e504ad145c3451258d8ab5f49d75fe1006436
oduodg <werewildfire@gmail.com>
2022-11-08 00:45:08 +0900

    :lipstick: Style Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
+import { Header } from './components/common';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p className="font-Kyobo">
-          Hello, 먼지구름!
-        </p>
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/common/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import AppIcon from '../../assets/images/munji.png';
+import { ReactComponent as MenuIcon } from '../../assets/icons/menu.svg';
+
+export default function header() {
+	return (
+		<div className='flex justify-between px-56 py-4 w-full h-30 bg-white outline outline-1 outline-[#cccccc]'>
+			<div className='appTitle'>
+				<img className="w-10 inline" alt="appIcon" src={AppIcon}></img>
+				<span className='font-Kyobo px-2 text-2xl text-[#272727]'>먼지구름</span>
+			</div>
+
+			<div className='menu'>
+				<div className='px-10 inline-block align-middle'>
+				<button className='mx-3 font-semibold text-[#272727]'>대기오염 현황</button>
+				<button className='mx-3 font-semibold text-[#838383]'>지도로 보기</button>
+				<button className='mx-3 font-semibold text-[#838383]'>실시간 영상</button>
+				</div>
+				<MenuIcon className='inline w-5'/>
+			</div>
+		</div>
+
+		
+	)
+}
