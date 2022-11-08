7c6d37ba87a861a68a851ab215350e743f98e39e
oduodg <werewildfire@gmail.com>
2022-11-08 21:20:01 +0900

    🎨 export ForecastBox

--- src/components/airPollution/index.js
+++ src/components/airPollution/index.js
@@ -1,3 +1,6 @@
 export { MainBox } from './MainBox';
 export { InstructionBox } from './InstructionBox';
 export { PollutantBox } from './PollutantBox';
+export { HourlyForecastBox } from './HourlyForecastBox';
+export { DailyForecastBox } from './DailyForecastBox';
+export { WholeAirInfoBox } from './WholeAirInfoBox';
