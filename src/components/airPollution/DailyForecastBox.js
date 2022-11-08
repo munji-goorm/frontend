a86f53a52e92bd327d0538b6e10739d342cbc448
oduodg <werewildfire@gmail.com>
2022-11-08 21:15:47 +0900

    💄 Add DailyForecastBox and HourlyForecastBox

---
+++ src/components/airPollution/DailyForecastBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const DailyForecastBox = () => {
+	return (
+		<div className='w-[31rem] h-[30rem] bg-[#ffffff] rounded-md my-[0.5rem]'>
+			<div className='p-5 pb-10 text-lg font-bold text-[#272727]'>일간 예보</div>
+		</div>
+	)
+}
---
+++ src/components/airPollution/HourlyForecastBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HourlyForecastBox = () => {
+	return (
+		<div className='w-[63rem] h-[15rem] bg-[#ffffff] rounded-md my-[0.5rem]'>
+				<div className='p-5 pb-10 text-lg font-bold text-[#272727]'>주간별 예보</div>
+		</div>
+	)
+}
