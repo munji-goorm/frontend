4b662fa9a74916f946b0d5f7091705b4152e44f5
oduodg <werewildfire@gmail.com>
2022-11-08 18:52:24 +0900

    💄 update Header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import AppIcon from '../../assets/images/goodMunji.png';
-import { ReactComponent as MenuIcon } from '../../assets/icons/menu.svg';
 
 export default function header() {
 	return (
-		<div className='flex justify-between px-56 py-4 w-full h-30 bg-white outline outline-1 outline-[#cccccc]'>
-			<div className='appTitle'>
-				<img className="w-10 inline" alt="appIcon" src={AppIcon}></img>
-				<span className='font-Kyobo px-2 text-2xl text-[#272727]'>먼지구름</span>
+		<div className='flex flex-col h-[4rem] items-center justify-center bg-white outline outline-1 outline-[#cccccc]'>
+			<div className='flex flex-row w-[63rem] h-[3rem]items-center justify-between'>
+				<div className='flex flex-row items-center'>
+					<img className="inline w-10" alt="appIcon" src={AppIcon}></img>
+					<span className='font-Kyobo px-2 text-2xl text-[#272727]'>먼지구름</span>
+				</div>
+
+				<div className='flex flex-row items-center'>
+					<button className='h-[2rem] px-[0.7rem] flex items-center font-semibold text-[#272727]'>대기오염 현황</button>
+					<button className='h-[2rem] px-[0.7rem] flex items-center font-base text-[#838383]'>지도로 보기</button>
+					<button className='h-[2rem] px-[0.7rem] flex items-center font-base text-[#838383]'>실시간 영상</button>
+
+				</div>
 			</div>
 
-			<div className='menu'>
-				<div className='px-10 inline-block align-middle'>
-				<button className='mx-3 font-semibold text-[#272727]'>대기오염 현황</button>
-				<button className='mx-3 font-semibold text-[#838383]'>지도로 보기</button>
-				<button className='mx-3 font-semibold text-[#838383]'>실시간 영상</button>
-				</div>
-				<MenuIcon className='inline w-5'/>
-			</div>
 		</div>
 
-		
+
 	)
 }
