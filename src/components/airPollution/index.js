c1add1316fca7a0cb39e510fc03613d7af6f9d6e
oduodg <werewildfire@gmail.com>
2022-11-10 12:42:29 +0900

    🔥 Remove HourlyForecastBox

--- src/components/airPollution/index.js
+++ src/components/airPollution/index.js
@@ -1,8 +1,7 @@
 export { MainBox } from './MainBox';
 export { InstructionBox } from './InstructionBox';
 export { PollutantBox } from './PollutantBox';
-export { HourlyForecastBox } from './HourlyForecastBox';
 export { DailyForecastBox } from './DailyForecastBox';
 export { WholeAirInfoBox } from './WholeAirInfoBox';
-export { OneHourForecast } from './OneHourForecast';
 export { GradeButton } from './GradeButton';
+export { RegionAirBtn } from './RegionAirBtn'
--- src/pages/AirPollution.js
+++ src/pages/AirPollution.js
@@ -1,22 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import UserLocation from '../apis/UserLocation';
-import { DailyForecastBox, HourlyForecastBox, MainBox, PollutantBox, WholeAirInfoBox } from '../components/airPollution';
+import { DailyForecastBox, MainBox, PollutantBox, WholeAirInfoBox } from '../components/AirPollution';
 import { ReactComponent as LocationIcon } from '../assets/icons/location.svg';
-import { ReactComponent as DropDownArrowIcon } from '../assets/icons/dropDownArrow.svg'
+import { ReactComponent as DropDownArrowIcon } from '../assets/icons/dropDownArrow.svg';
+import { ReactComponent as SearchIcon } from '../assets/icons/search.svg';
 
 export default function AirPollution() {
 	//const userLocation = UserLocation();
 
 	return (
 		<div className='flex flex-col items-center'>
-			<div className='flex justify-center mt-[1rem] mb-[0.5rem] userLocation'>
-				<LocationIcon className='inline w-6' />
-				<div className='ml-3 mr-2 text-xl font-semibold text-[#272727]'>마포구 연남동</div>
-				<DropDownArrowIcon className='inline w-6' />
-			</div>
-
 			<MainBox />
-			<HourlyForecastBox />
+			
 			<div className='w-[63rem] flex flex-row justify-between'>
 				<DailyForecastBox />
 				<WholeAirInfoBox />
