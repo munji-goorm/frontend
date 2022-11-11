e09fd5fcd21cf32e3ea308e5e29a674fbe69d661
oduodg <werewildfire@gmail.com>
2022-11-11 17:42:43 +0900

    ✨ move center of Map

--- src/components/OntheMap/MapContainer.js
+++ src/components/OntheMap/MapContainer.js
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react';
+import { GetLocation } from '../../apis';
+
 
 export const MapContainer = () => {
 
 	const { kakao } = window;
 
+	let coord = GetLocation();
+	// console.log(coord);
+
 	useEffect(() => {
 		const container = document.getElementById('kakaoMap'); // 지도를 표시할 div
 		const options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-			level: 4 // 지도의 확대 레벨
+			center: new kakao.maps.LatLng(37.584009, 126.970626), // 지도의 중심좌표
+			level: 6 // 지도의 확대 레벨
 		};
 		const map = new kakao.maps.Map(container, options); // 지도를 생성합니다.
-	}, []);
+
+		// 이동할 위도 경도 위치를 생성합니다
+		var moveLatLon = new kakao.maps.LatLng(coord.lat, coord.lng);
+		// 지도 중심을 이동 시킵니다
+		map.setCenter(moveLatLon);
+		
+	}, [coord]);
 
 	return (
 		<div id='kakaoMap'
