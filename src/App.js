429f60ced4b274b9f162e41b64bb4e43faa70aed
oduodg <werewildfire@gmail.com>
2022-11-12 15:41:06 +0900

    🎨 코드 정리

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header, Footer } from './components/common';
 import { AirPollution, LiveCam, OntheMap } from './pages';
-import { UseGeolocation, CoordinateTM } from './apis';
 
 function App() {
-  // const location = UseGeolocation();
-  // const tm = CoordinateTM();
-  // console.log(tm);
 
   return (
     <BrowserRouter>
       <Header />
-      {/* {location.loaded 
-      ? JSON.stringify(location)
-      : "Location data not available yet."} */}
       <Routes>
         <Route exact path="/" element={<AirPollution />} />
         <Route path="/map" element={<OntheMap />} />
