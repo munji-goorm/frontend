7dc4a99f7d1ad0bfeef377862bcfacfae9249957
oduodg <werewildfire@gmail.com>
2022-11-14 11:52:01 +0900

    ⚡️ 오염 물질 버튼 클릭 시 측정소 위치와 해당 수치/등급 마커&커스텀 오버레이 표시

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,3 +2,4 @@ export { Coord2TM } from './Coord2TM';
 export { Coord2address } from './Coord2address';
 export { GetStationName } from './GetStationName';
 export { GetLocation } from './GetLocation';
+export { GetStationInfo } from './GetStationInfo';
--- src/components/OntheMap/StationMap.js
+++ src/components/OntheMap/StationMap.js
@@ -1,62 +1,22 @@
 import React, { useEffect } from 'react';
-import { GetLocation } from '../../apis';
+import { GetLocation, GetStationInfo } from '../../apis';
 
-export const StationMap = () => {
-
+export const StationMap = ({ name }) => {
 	const { kakao } = window;
-
 	let coord = GetLocation();
 	// console.log(coord);
 
-	// 마커를 표시할 위치와 title 객체 배열입니다 
-	var positions = [
-		{
-			title: '선유도공원',
-			latlng: new kakao.maps.LatLng(37.5444, 126.8999),
-		},
-		{
-			title: '양화한강공원',
-			latlng: new kakao.maps.LatLng(37.5395, 126.9018)
-		}
-	];
+	const stationData = GetStationInfo();
+	//console.log(stationData);
 
-let val = 12;
-
-let val2 = 0.059;
-
-var content = [
-	{
-		ctn: '<div style="color: white; font-size:1rem">' +
-		`    <span class="title">${val}</span>` +
-		'</div>'
-	}, 
-	{
-		ctn: '<div style="color: white; font-size:1rem">' +
-		`    <span class="title">${val2}</span>` +
-		'</div>'
-	}
-]
-
-	// 마커 이미지의 이미지 주소입니다
-	var imageSrc = process.env.PUBLIC_URL + '/images/markerblue.png';
-	// 마커 이미지의 이미지 크기 입니다
-	var imageSize = new kakao.maps.Size(50, 35);
-
-	// 마커 이미지를 생성합니다
-	var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-
-	// 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-	// var content = '<div style="color: white; font-size:1rem">' +
-	// 	`    <span class="title">${val}</span>` +
-	// 	'</div>';
-
+	let grade;
+	let val;
 
 	useEffect(() => {
 		const container = document.getElementById('kakaoMap'); // 지도를 표시할 div
 		const options = {
-			center: new kakao.maps.LatLng(37.584009, 126.970626), // 지도의 중심좌표
-			level: 6 // 지도의 확대 레벨
+			center: new kakao.maps.LatLng(37.564639, 126.975961), // 지도의 중심좌표
+			level: 7 // 지도의 확대 레벨
 		};
 		const map = new kakao.maps.Map(container, options); // 지도를 생성합니다.
 
@@ -66,25 +26,69 @@ var content = [
 		map.setCenter(moveLatLon);
 
 
-		for (var i = 0; i < positions.length; i++) {
+		// 마커를 표시할 위치와 title 객체 배열입니다.
+		for (let i = 0; i < stationData.length; i++) {
+			if (name == "CAI") {
+				grade = stationData[i].khaiState;
+				val = stationData[i].khaiValue;
+			} else if (name == "PM10"){
+				grade = stationData[i].pm10State;
+				val = stationData[i].pm10Value;
+			} else if (name == "PM25"){
+				grade = stationData[i].pm25State;
+				val = stationData[i].pm25Value;
+			} else if (name == "SO2"){
+				grade = stationData[i].so2State;
+				val = stationData[i].so2Value;
+			} else if (name == "NO2"){
+				grade = stationData[i].no2State;
+				val = stationData[i].no2Value;
+			} else if (name == "CO"){
+				grade = stationData[i].coState;
+				val = stationData[i].coValue;
+			} else if (name == "O3"){
+				grade = stationData[i].o3State;
+				val = stationData[i].o3Value;
+			} 
+
+			let imageSrc;
+			if (grade == "좋음") {
+				imageSrc = process.env.PUBLIC_URL + '/images/markerblue.png';
+			} else if (grade == "보통") {
+				imageSrc = process.env.PUBLIC_URL + '/images/markerGreen.png';
+			} else if (grade == "나쁨") {
+				imageSrc = process.env.PUBLIC_URL + '/images/markerOrange.png';
+			} else if (grade == "최악"){
+				imageSrc = process.env.PUBLIC_URL + '/images/markerRed.png';
+			} else { //점검중
+				imageSrc = process.env.PUBLIC_URL + '/images/markerGray.png';
+			}
+			if (val == -1) {
+				val = "점검중";
+			}
+
+			// 마커를 생성합니다.
 			var marker = new kakao.maps.Marker({
 				map: map, // 마커를 표시할 지도
-				position: positions[i].latlng, // 마커를 표시할 위치
-				title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다.
-				image: markerImage,
+				position: new kakao.maps.LatLng(stationData[i].dmX, stationData[i].dmY), // 마커를 표시할 위치
+				title: stationData[i].stationName, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다.
+				image: new kakao.maps.MarkerImage(imageSrc, new kakao.maps.Size(50, 35)),
 			});
 
-			// 커스텀 오버레이를 생성합니다
+			// 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+			let content = '<div style="color: white; font-size:1rem">' +
+				`<span class="title">${val}</span>` +
+				'</div>';
+
+			// 커스텀 오버레이를 생성합니다 
 			var customOverlay = new kakao.maps.CustomOverlay({
 				map: map,
-				position: positions[i].latlng,
-				content: content[i].ctn,
+				position: new kakao.maps.LatLng(stationData[i].dmX, stationData[i].dmY),
+				content: content,
 				yAnchor: 1.34
 			});
-
 		}
-
-	}, [coord]);
+	});
 
 	return (
 		<div id='kakaoMap'
