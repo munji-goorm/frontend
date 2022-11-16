dd30a733510a30e91a5961d449d6586b8eb1368c
oduodg <werewildfire@gmail.com>
2022-11-16 11:07:31 +0900

    ✏️ 폴더명 대소문자 구분 airPollution -> AirPollution

---
+++ src/components/AirPollution/HitmapBox.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+export const HitmapBox = ({ name, val }) => {
+	
+	return (
+		<div className='w-[31rem] flex flex-col justify-center h-[17rem] bg-[#ffffff] rounded-md my-[0.5rem]'>
+			<div className='p-5 text-base font-bold text-[#272727]'>{name}</div>
+			<div className='w-[31rem] flex justify-center px-5'>
+				<iframe className='rounded-lg w-[29rem] h-[12rem] mb-5' src={`https://earth.nullschool.net/ko/#current/particulates/surface/level/overlay=${val}/orthographic=127.868,37.20,1300`}></iframe>
+			</div>
+		</div>
+	)
+}
