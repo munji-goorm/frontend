b606d134fae3bd6791523720be9d1801d2251f40
oduodg <werewildfire@gmail.com>
2022-11-16 11:12:18 +0900

    ✏️ 폴더명 대소문자 구분 airPollution -> AirPollution

--- src/components/airPollution/index.js
+++
@@ -1,8 +0,0 @@
-export { MainBox } from './MainBox';
-export { InstructionBox } from './InstructionBox';
-export { PollutantBox } from './PollutantBox';
-export { DailyForecastBox } from './DailyForecastBox';
-export { WholeAirInfoBox } from './WholeAirInfoBox';
-export { GradeButton } from './GradeButton';
-export { RegionAirBtn } from './RegionAirBtn';
-export { HitmapBox } from './HitmapBox';
