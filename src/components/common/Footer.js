a7536c77957401bd43879f27af8e175f0bf4c8dc
oduodg <werewildfire@gmail.com>
2022-11-28 18:15:39 +0900

    💄 footer에 개인 깃허브 링크 및 노션 링크 추가

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,73 +1,82 @@
 import React from 'react';
 import AppIcon from '../../assets/images/goodMunji.png';
-import { ReactComponent as GithubIcon } from '../../assets/icons/github.svg'
+import { ReactComponent as GithubIcon } from '../../assets/icons/github.svg';
+import { ReactComponent as NotionIcon } from '../../assets/icons/notion.svg';
 
 export default function Footer() {
 	return (
-		<div className='flex flex-col h-[22rem] mt-[5rem] bg-[#ffffff] outline outline-1 outline-[#cccccc] items-center justify-center text-[#272727]'>
+		<div className='flex flex-col h-[25rem] mt-[5rem] bg-[#ffffff] outline outline-1 outline-[#cccccc] items-center justify-center text-[#272727]'>
 			<div className='flex flex-col w-[63rem] h-[9rem] justify-center'>
 				<div className='flex flex-row items-center h-[4rem]'>
 					<img className="w-10" alt="appIcon" src={AppIcon}></img>
-					<div className='text-2xl ml-[0.5rem]' style={{fontFamily:"LeeSeoyun"}}>만든 사람</div>
+					<div className='text-2xl ml-[0.5rem]' style={{ fontFamily: "LeeSeoyun" }}>만든 사람</div>
 				</div>
-				<div className='pt-1 text-lg' style={{fontFamily:"LeeSeoyun"}}>
-					예쁘고 귀엽게 봐주세요. 감사합니다 :)
+				<div className='pt-1 text-lg' style={{ fontFamily: "LeeSeoyun" }}>
+					예쁘고 귀엽게 봐주세요. 감사합니다 :) <br />
+					이름을 클릭하면 개인 깃허브 링크로 이동합니다.
 				</div>
 			</div>
 
 			<div className='w-[63rem] h-[9rem] flex flex-row justify-between'>
-				<div className='flex flex-row '>
-					<div className='mr-[4rem]'>
-						<div className='mb-[0.2rem] text-lg' style={{fontFamily:"LeeSeoyun"}}>
+				<div className='flex flex-row w-[27rem] justify-between'>
+					<div className=''>
+					<a href='https://github.com/oduodg' target="_blank" rel="noreferrer">
+						<div className='mb-[0.2rem] text-lg hover:text-[#8A67AB] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
 							Eun <br /> Lee
 						</div>
-						<div className='text-xs'>
-							Team Leader <br />
+						</a>
+						<div className='flex items-center'>
+							<div className='text-xs'>
+								Team Leader <br />
+							</div>
 						</div>
 					</div>
-					<div className='mr-[4rem]'>
-						<div className='text-lg' style={{fontFamily:"LeeSeoyun"}}>
+					<div className=''>
+					<a href='https://github.com/ksee1230' target="_blank" rel="noreferrer">
+						<div className='mb-[0.2rem] text-lg hover:text-[#548164] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
 							Kyungsoo <br /> Park
 						</div>
+						</a>
 					</div>
-					<div className='mr-[4rem]'>
-						<div className='text-lg' style={{fontFamily:"LeeSeoyun"}}>
+					<div className=''>
+					<a href='https://github.com/Zemni98' target="_blank" rel="noreferrer">
+						<div className='mb-[0.2rem] text-lg hover:text-[#5081A9] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
 							Jaemin <br /> Shin
 						</div>
+						</a>
 					</div>
-					<div className='mr-[4rem]'>
-						<div className='text-lg' style={{fontFamily:"LeeSeoyun"}}>
+					<div className=''>
+					<a href='https://github.com/soyeonnn' target="_blank" rel="noreferrer">
+						<div className='mb-[0.2rem] text-lg hover:text-[#C29343] hover:font-bold hover:rotate-3' style={{ fontFamily: "LeeSeoyun" }}>
 							Soyeon <br /> Hwang
 						</div>
+						</a>
 					</div>
 				</div>
 
-				<div className='flex text-2xl' style={{fontFamily:"LeeSeoyun"}}>
+				<div className='flex text-2xl' style={{ fontFamily: "LeeSeoyun" }}>
 					kakao cloud school <br />
 					Dev 1th. <br />
 					Final Project
 				</div>
 			</div>
-
 			<div className='w-[63rem] h-[7rem] flex flex-col'>
 				<div className='flex flex-col items-center justify-center'>
 					<div className='flex flex-row'>
-						<div className='mx-[0.5rem]'>
+						<div className='mx-[0.5rem] flex'>
 							<a href='https://github.com/munji-goorm' target="_blank" rel="noreferrer">
-							<GithubIcon />
+								<GithubIcon className='mx-[0.3rem]' />
+							</a>
+							<a href='https://oduodg.notion.site/munji-goorm-1edd7fd5820d463481096403530dbda0' target="_blank" rel="noreferrer">
+								<NotionIcon className='mx-[0.3rem]' />
 							</a>
 						</div>
 					</div>
-
 					<div className='text-sm text-center text-[#838383] pt-[1rem]'>
 						ⓒ 2022 kakao cloud school Dev 1th. Team2 - Allrights reserved.
 					</div>
-
 				</div>
-
 			</div>
-
-
 		</div>
 	)
 }
