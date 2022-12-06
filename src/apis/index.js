4f2141cf9306aef3f5db3cf5c5c11b9e88add894
oduodg <werewildfire@gmail.com>
2022-12-06 09:01:10 +0900

    🔥 불필요한 코드 및 파일 삭제

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,3 @@
 export { GetMainData } from './GetMainData';
-export { GetLocation } from './GetLocation';
 export { GetStationInfo } from './GetStationInfo';
 export { GetCCTVInfo } from './GetCCTVInfo';
-export { Coord2Addr } from './Coord2Addr';
